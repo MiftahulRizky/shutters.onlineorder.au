daf1a5d6e20ec0cf056c8db24ec6c50b1ce46d9e
rzndkprtm <rzndkprtm@gmail.com>
2026-01-28 10:08:38 +0700

    Update Gap lagi

--- Order/Method.aspx.vb
+++ Order/Method.aspx.vb
@@ -1785,10 +1785,11 @@ Partial Class Order_Method
                 data.buildoutposition = String.Empty
             End If
 
-            If layoutCode.Contains("T") AndAlso layoutCode.Contains("B") AndAlso layoutCode.Contains("C") AndAlso layoutCode.Contains("G") Then
+            If layoutCode.Contains("T") OrElse layoutCode.Contains("B") OrElse layoutCode.Contains("C") OrElse layoutCode.Contains("G") Then
                 data.samesizepanel = String.Empty
                 gap1 = 0 : gap2 = 0 : gap3 = 0 : gap4 = 0 : gap5 = 0
             End If
+
 
             If data.samesizepanel = "Yes" Then
                 gap1 = 0 : gap2 = 0 : gap3 = 0 : gap4 = 0 : gap5 = 0
